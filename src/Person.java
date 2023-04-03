cc721b7fb1f9daafa4e4f916379f723d79884560
Shruti <shruti@simformsolutions.com>
2023-04-03 13:56:10 +0530

    Update

--- src/Person.java
+++ src/Person.java
@@ -1,7 +1,8 @@
-class Person{
+class Person {
     String name;
     int id;
-    Person(int id, String name){
+
+    Person(int id, String name) {
         this.id = id;
         this.name = name;
     }
